12c875a0df3b7378daaa46ffb19cde8bbcc71b4e
Yoel Nunez <y.nunez@floms.com>
2018-01-24 15:44:12 -0600

    fixed JS lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
+const gulp = require("gulp");
+const mocha = require("gulp-mocha");
 
-gulp.task('test', function(){
-  return gulp.src('src/**/*.spec.ts')
+gulp.task("test", function(){
+  return gulp.src("src/**/*.spec.ts")
         .pipe(mocha({
-            reporter: 'nyan',
-            require: ['ts-node/register']
+            reporter: "nyan",
+            require: ["ts-node/register"]
         }));
 });
 
-gulp.task('default', [ 'test' ]);
+gulp.task("default", [ "test" ]);
